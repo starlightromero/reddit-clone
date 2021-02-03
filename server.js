8d063af948ce818f7be1d99357ffa057325c164d
Starlight Romero <starlightromero@protonmail.com>
2021-02-02 17:03:06 -0800

    [update] added hbs view engine and routes

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
-const express = require('express')
-const app = express()
-const port = 3000
 const path = require('path')
+const express = require('express')
+const handlebars = require('express-handlebars')
+const routes = require('./routes/main')
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/index.html'))
-})
+const port = 3000
+const app = express()
+
+app.set('view engine', 'hbs')
+app.engine('hbs', handlebars({
+  layoutsDir: path.join(__dirname, '/views/layouts/'),
+  partialsDir: path.join(__dirname, '/views/partials/'),
+  extname: 'hbs',
+  defaultLayout: 'base'
+}))
+
+app.use(routes)
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Server running on port ${port}`)
 })
