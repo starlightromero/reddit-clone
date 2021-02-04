d36e1cb39c5e412e4d315ec7861a76ae9c01ad42
Starlight Romero <starlightromero@protonmail.com>
2021-02-03 20:35:20 -0800

    [update] added getSubreddit route

--- routes/main.js
+++ routes/main.js
@@ -3,6 +3,7 @@ const router = express.Router()
 
 const mainController = require('../controllers/main')
 
+router.get('/r/:subreddit', mainController.getSubreddit)
 router.get('/', mainController.index)
 
 module.exports = router
