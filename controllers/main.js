1c5bec5b8e4a01aeb0608f6502b3ed13e68f407a
Starlight Romero <starlightromero@protonmail.com>
2021-02-03 20:33:49 -0800

    [update] added getSubreddit controller

--- controllers/main.js
+++ controllers/main.js
@@ -1,3 +1,15 @@
+const Post = require('../models/post')
+
 exports.index = (req, res) => {
   return res.render('index')
+}
+
+exports.getSubreddit = (req, res) => {
+  Post.find(
+    { subreddit: req.params.subreddit }
+  ).lean().then(posts => {
+    res.render('posts-index', { posts })
+  }).catch(err => {
+    console.log(err)
+  })
 }
