b5f5e4ab19af31ca196c14784eb282d5da6546fd
Starlight Romero <starlightromero@protonmail.com>
2021-03-01 16:49:51 -0800

    Implemented initial authentication tests

--- test/index.js
+++ test/index.js
@@ -7,15 +7,18 @@ chai.use(chaiHttp)
 
 describe('site', function () {
   it('Should have home page', function (done) {
-    chai
-      .request(app)
-      .get('/')
-      .end(function (err, res) {
+    chai.request(
+      app
+    ).get(
+      '/'
+    ).end(
+      function (err, res) {
         if (err) {
           return done(err)
         }
         res.status.should.be.equal(200)
         return done()
-      })
+      }
+    )
   })
 })
